59fd21f7fdecdea8d8fd162840897dd5aded3383
mvindas13 <mvindasr@ucenfotec.ac.cr>
2021-08-03 22:40:59 -0600

    Commit final de Parte 1 y 2 de Lab 9 - Componentes

---
+++ src/main/java/com/cenfotec/seguridad/MemorySecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.cenfotec.seguridad;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+public class MemorySecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws
+            Exception {
+        String encodedPassword = new BCryptPasswordEncoder().encode("mario");
+
+        auth.inMemoryAuthentication()
+                .withUser("mario")
+                .password(encodedPassword)
+                .roles("manager", "dev")
+                .and().passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+}
